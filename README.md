836bcdf9cd931eeccfe3fac6ba4c938d1227baa7
Sanensi <william.picard@hotmail.com>
2021-01-26 10:58:19 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Qohash
+
+Technical homework for Qohash
---
+++ src/main/java/sanensi/App.java
@@ -0,0 +1,7 @@
+package sanensi;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
